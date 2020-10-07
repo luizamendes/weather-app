be7093bb20ec15c26c4e87cb32fdaa1f39d25c22
Luiza Mendes <luizaleitemendes@gmail.com>
2020-10-07 16:25:34 -0300

    Changing API URL to https

--- src/api/weather.js
+++ src/api/weather.js
@@ -4,7 +4,7 @@ const openApiKey = process.env.REACT_APP_OPENAI_KEY;
 
 const fetchWeather = (lat, long) =>
   axios.get(
-    `http://api.openweathermap.org/data/2.5/find?lat=${lat}&lon=${long}&cnt=15&APPID=${openApiKey}&units=metric`
+    `https://api.openweathermap.org/data/2.5/find?lat=${lat}&lon=${long}&cnt=15&APPID=${openApiKey}&units=metric`
   );
 
 export { fetchWeather };
